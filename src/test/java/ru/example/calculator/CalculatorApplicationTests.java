8bc5d54c591f0b8081cc2a189371d511e7751eb7
Максим Евсеев <vailogiant@gmail.com>
2023-05-03 22:24:51 +0500

    git init

---
+++ src/test/java/ru/example/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.example.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
